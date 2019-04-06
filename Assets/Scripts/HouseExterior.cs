b76a0af9e77f6edc06ceaf2986c8b6d44642d868
Luis Correa <luiscorrea9614@gmail.com>
2019-04-06 12:08:54 -0600

    Multiple changes, inside scene, outside house and display logic

---
+++ Assets/Scripts/HouseExterior.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HouseExterior : MonoBehaviour
+{
+    public int IndexScene;
+    public Text MessageDisplay;
+
+    // bEHAVIOUR vARIABLE
+    bool isInTriggerZone = false;
+    // Control Variables
+    float lastStep = 0.0f, timeBetweenSteps = 0.5f;
+    
+    void Start()
+    {
+        displayMessage("loool");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInTriggerZone)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                if (Time.time - lastStep > timeBetweenSteps)
+                {
+                    lastStep = Time.time;
+                    SceneManager.LoadScene(IndexScene);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        displayMessage("Entered");
+        isInTriggerZone = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInTriggerZone = false;
+        hideMessage();
+    }
+    void displayMessage(string msg)
+    {
+        MessageDisplay.text = msg;
+        //MessageDisplay.SetActive(true);
+    }
+
+    void hideMessage()
+    {
+        MessageDisplay.text = "";
+    }
+}
